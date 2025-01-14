8c9c1f52169c184c9ba375814e88b2a60115b6e3
scriptPilot <dennis@scheffel.de>
2025-01-14 20:00:43 +0100

    using prettier

--- buildscript.js
+++ buildscript.js
@@ -1,48 +1,46 @@
 // Import File System module
-const fs = require('fs')
+const fs = require("fs");
 
 // Read library folder
-fs.readdir('lib', (err, files) => {
-
+fs.readdir("lib", (err, files) => {
   // Exit on error
   if (err) {
-    console.error('Failed to loop the library folder.', err)
-    process.exit(1)
+    console.error("Failed to loop the library folder.", err);
+    process.exit(1);
   }
 
   // Filter for .js files
-  files = files.filter(file => /\.js$/.test(file))
+  files = files.filter((file) => /\.js$/.test(file));
 
   // Filter out onCalendarUpdate.js file
-  files = files.filter(file => file !== 'onCalendarUpdate.js')
+  files = files.filter((file) => file !== "onCalendarUpdate.js");
 
   // Start array of code blocks
-  const codeBlocks = []
+  const codeBlocks = [];
 
   // Add date and link
-  const now = new Date()
-  const date = now.toISOString().substr(0, 10)
-  const link = 'https://github.com/scriptPilot/google-calendar-synchronization'
-  codeBlocks.push(`// Google Calendar Synchronization, build on ${date}\n// Source: ${link}`)
+  const now = new Date();
+  const date = now.toISOString().substr(0, 10);
+  const link = "https://github.com/scriptPilot/google-calendar-synchronization";
+  codeBlocks.push(
+    `// Google Calendar Synchronization, build on ${date}\n// Source: ${link}`,
+  );
 
   // Loop files
-  files.forEach(file => {
-
+  files.forEach((file) => {
     // Read file content
-    const fileContent = fs.readFileSync('lib/' + file, { encoding: 'utf8' })
+    const fileContent = fs.readFileSync("lib/" + file, { encoding: "utf8" });
 
     // Add file content to code blocks
-    codeBlocks.push(fileContent)
-
-  })
+    codeBlocks.push(fileContent);
+  });
 
   // Create a dist folder if not exist
-  if (!fs.existsSync('dist')) fs.mkdirSync('dist')
+  if (!fs.existsSync("dist")) fs.mkdirSync("dist");
 
   // Merge and write code blocks to Code.gs file
-  fs.writeFileSync('dist/Code.gs', codeBlocks.join('\n\n'))
+  fs.writeFileSync("dist/Code.gs", codeBlocks.join("\n\n"));
 
   // Log script completion
-  console.log('Code.gs file updated.')
-
-})
+  console.log("Code.gs file updated.");
+});
