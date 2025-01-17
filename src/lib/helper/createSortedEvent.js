818646c116dc748cfd03e2ae7789157a81e06a7c
scriptPilot <dennis@scheffel.de>
2025-01-17 17:03:08 +0100

    Update createSortedEvent.js

--- src/lib/helper/createSortedEvent.js
+++ src/lib/helper/createSortedEvent.js
@@ -5,16 +5,16 @@ function createSortedEvent(obj) {
     const sortedKeys = Object.keys(obj).sort();
     sortedKeys.forEach((key) => {
       // Harmonize dateTime string
-      if (key === 'dateTime') {
-        sortedObj[key] = new Date(obj[key]).toISOString()
-      // Sort recurrence elements properly
-      } else if (key === 'recurrence') {
-        sortedObj[key] = obj[key].map(el => {
-          let [ elKey, elValue ] = el.split(':')
-          elValue = elValue.split(';').sort().join(';')
-          return [ elKey, elValue ].join(':')
-        })
-      // Any other property
+      if (key === "dateTime") {
+        sortedObj[key] = new Date(obj[key]).toISOString();
+        // Sort recurrence elements properly
+      } else if (key === "recurrence") {
+        sortedObj[key] = obj[key].map((el) => {
+          let [elKey, elValue] = el.split(":");
+          elValue = elValue.split(";").sort().join(";");
+          return [elKey, elValue].join(":");
+        });
+        // Any other property
       } else {
         sortedObj[key] = createSortedEvent(obj[key]);
       }
